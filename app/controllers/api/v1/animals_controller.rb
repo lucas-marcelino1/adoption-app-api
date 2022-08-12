d446b8f4c2885bda756b95d668ff7a4e39415555
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-08-12 16:19:10 -0300

    add user and config devise_token_auth

--- app/controllers/api/v1/animals_controller.rb
+++ app/controllers/api/v1/animals_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::AnimalsController < ActionController::API
+class Api::V1::AnimalsController < ApplicationController
   def index
     @animals = Animal.all
     render status: :ok, json: @animals
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth'
     namespace :v1 do
       resources :animals, only: %i[index]
     end
