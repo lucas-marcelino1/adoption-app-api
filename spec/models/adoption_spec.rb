705f6ae2e7a4af277610a0ef54c99d8d31d7c8d8
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-08-20 11:48:39 -0300

    model tests to adoption

--- spec/models/adoption_spec.rb
+++ spec/models/adoption_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Adoption, type: :model do
-  
+  describe '#valid?' do
+    it 'presence of title' do
+      adoption = build(:adoption, title: '')
+      expect(adoption).not_to be_valid
+    end
+
+    it 'presence of description' do
+      adoption = build(:adoption, description: '')
+      expect(adoption).not_to be_valid
+    end
+
+    it 'length of title' do
+      adoption = build(:adoption, title: 'DIUWAHUHIWDAUHWIDAWDHUIADWUHWWUHDWADWAWDADW')
+      expect(adoption).not_to be_valid
+    end
+  end
 end
